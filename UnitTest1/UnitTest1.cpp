d0f8476bed6c1267f4eb48c79a7134948da7f802
Lenovo <Desofiyamariya@gmail.com>
2022-06-17 02:07:37 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[] = { 1, -1, 0 };
+			Assert::AreEqual(1, accumulate(&a[0], &a[1]));
+		}
+	};
+}
